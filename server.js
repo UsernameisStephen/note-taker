6bca8666e58fe17d657aa44cf57ef3c6e91ea5bd
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-03 15:10:47 -0500

    cleaned code to get heroku working properly

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const PORT = process.env.PORT || 3001;
-
-const path = require ('path');
 const express = require('express');
 const fs = require('fs');
-const notes = require("./Develop/db/db.json");
+const notes = require('./Develop/db/db.json');
 const uuid = require('uuid');
+const path = require ('path');
 
 const app = express();
-
+const PORT = process.env.PORT || 3001;
 
 //middleware for pairing json
 app.use(express.json());
@@ -15,29 +13,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //route for notes
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/Develop/db/db.json"))
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Develop/db/db.json'))
 });
 //route for homepage
-app.post("/api/notes", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("./Develop/db/db.json"));
+app.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./Develop/db/db.json'));
     const newNotes = req.body;
     newNotes.id = uuid.v4();
     notes.push(newNotes);
-    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes))
+    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(notes))
     res.json(notes);
 });
 
 //calls for home page and the notes.html
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
 });
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
 });
-
 //app listener
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+app.listen(PORT, () => {
+    console.log(`Server has started. Listening at http://localhost:${PORT}`)
 });
