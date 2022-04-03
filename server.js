4785afbb897f78799b909c308011085cc1b30cd9
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-02 22:40:57 -0500

    added a create notes function and middleware

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 const PORT = process.env.PORT || 3001;
+
+const path = require ('path');
 const express = require('express');
 const app = express();
 
-const notes = require('./db/db');
+
+const createdNotes = require('./db/db.json');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+function createNewNote(body, noteArray) {
+    const newNote = body;
+    if (!Array.isArray(noteArray))
+        noteArray = [];
+    
+    if (noteArray.length === 0)
+        noteArray.push(0);
+
+    body.id = noteArray[0];
+    noteArray[0]++;
+
+    noteArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(noteArray, null, 2)
+    );
+    return createdNotes;
+}
+
 
 
 app.get('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes)
     res.json(notes);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
