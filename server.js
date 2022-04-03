6ebb045921a699f66d952d6810bfa7e97fc154c2
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-03 14:54:46 -0500

    cleaned up code to get heroku working properly

--- server.js
+++ server.js
@@ -2,57 +2,42 @@ const PORT = process.env.PORT || 3001;
 
 const path = require ('path');
 const express = require('express');
-const app = express();
 const fs = require('fs');
+const notes = require("./Develop/db/db.json");
+const uuid = require('uuid');
+
+const app = express();
 
 
-const createdNotes = require('./db/db.json');
+//middleware for pairing json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-app.get('/api/notes', (req, res) => {
-    res.json(createdNotes.slice(1));
+//route for notes
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Develop/db/db.json"))
+});
+//route for homepage
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./Develop/db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes))
+    res.json(notes);
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+//calls for home page and the notes.html
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+//app listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });
-
-
-function createNewNote(body, noteArray) {
-    const newNote = body;
-    if (!Array.isArray(noteArray))
-        noteArray = [];
-    
-    if (noteArray.length === 0)
-        noteArray.push(0);
-
-    body.id = noteArray[0];
-    noteArray[0]++;
-
-    noteArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(noteArray, null, 2)
-    );
-    return newNote;
-}
-
-app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, createdNotes);
-    res.json(newNote);
-});
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-}); 
