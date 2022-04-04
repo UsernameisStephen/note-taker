ef1b654824ec4c0ffca963e6d65da833359f1de8
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-04 13:59:39 -0500

    cleaned up code and moved files

--- server.js
+++ server.js
@@ -1,40 +1,58 @@
+// Import our dependencies
 const express = require('express');
 const fs = require('fs');
-const notes = require('./Develop/db/db.json');
-const uuid = require('uuid');
-const path = require ('path');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// Setup our express specific variables
+const app = express()
+const port = process.env.port || 3000;
+
 
 //middleware for pairing json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('Devlop/public'));
 
-//route for notes
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/db/db.json'))
-});
-//route for homepage
-app.post('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./Develop/db/db.json'));
-    const newNotes = req.body;
-    newNotes.id = uuid.v4();
-    notes.push(newNotes);
-    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(notes))
-    res.json(notes);
-});
-
-//calls for home page and the notes.html
+//set route for homepage
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+  
+  // set route for notes
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  });
+  
+// Set our '/api/notes' route.
+app.get('/api/notes/', function (req, res) {
+    // Read the db file so we can put it on a page
+    fs.readFile(__dirname + '/db/db.json', (err, data) => {
+      // Parse our db.json data
+      var json = JSON.parse(data);
+      return res.json(json);
+    })
 });
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
+//catch-all to redirect to index.html
+app.get('*', function(req, res) {
+    res.sendFile((__dirname + '/public/index.html'));
 });
-//app listener
-app.listen(PORT, () => {
-    console.log(`Server has started. Listening at http://localhost:${PORT}`)
+
+// Express Routes 
+app.post("/api/notes/", function (req, res) {
+    newNote = req.body;
+  
+    // Get the JSON file from /db/ and parse it so we can add to it
+    fs.readFile(__dirname + "/db/db.json", (err, data) => {
+      var json = JSON.parse(data);
+      // Push our new note in from our user's request.
+      json.push(newNote);
+  
+      // Write the JSON file over with our new contents.
+      fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(json));
+    });
 });
+
+//Start our server app listener
+app.listen(PORT, () =>
+  console.info(`App listening at http://localhost:${PORT}`)
+);
